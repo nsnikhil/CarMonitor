0c2b62d9b60cf00cb403d2601c9ffad3d7042399
nsnikhil <n.nikhil.ns65@gmail.com>
2018-02-12 03:14:11 +0530

    update copyright

--- app/src/main/java/com/nsnik/nrs/carmonitor/views/fragments/LoginFragment.java
+++ app/src/main/java/com/nsnik/nrs/carmonitor/views/fragments/LoginFragment.java
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2018 nsnikhil
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.nsnik.nrs.carmonitor.views.fragments;
 
 
@@ -9,7 +25,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.jakewharton.rxbinding2.view.RxView;
@@ -19,7 +34,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Consumer;
 import timber.log.Timber;
 
 public class LoginFragment extends Fragment {
